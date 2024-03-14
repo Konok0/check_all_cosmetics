e5f95c59de54e5dcedca7e49234d91769ea66742
Konok0 <133997146+Konok0@users.noreply.github.com>
2024-03-14 22:36:39 +0500

    Update check_all_cosmetics_localization.lua

--- check_all_cosmetics/scripts/mods/check_all_cosmetics/check_all_cosmetics_localization.lua
+++ check_all_cosmetics/scripts/mods/check_all_cosmetics/check_all_cosmetics_localization.lua
@@ -7,8 +7,30 @@ return {
   },
   mod_see = {
     en = "Show all cosmetics",
+    ru = "Показать всю косметику",
+  },
+  mod_see_key = {
+    en = "Show all cosmetics",
+    ru = "Показать всю косметику",
   },
   mod_filter = {
     en = "Disable archetype filter",
+    ru = "Отключить фильтр классов",
+  },
+  mod_try = {
+    en = "Try it - function (!!!Better use a separate LOADOUTS!!!)",
+    ru = "Функция примерки (!!!Лучше использовать отдельную ВЫКЛАДКУ!!!)",
+  },
+  mod_try_key = {
+    en = "Try it - function (!!!Better use a separate LOADOUTS!!!)",
+    ru = "Функция примерки (!!!Лучше использовать отдельную ВЫКЛАДКУ!!!)",
+  },
+  mod_try_notif = {
+    en = "Try it - function",
+    ru = "Функция примерки",
+  },
+  mod_see_sysname = {
+    en = "Real (System) name for customization items in the customization menu",
+    ru = "Показывать системное имя предмета",
   },
 }
